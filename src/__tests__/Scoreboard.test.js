582abf08cc121794be70c30392844d3622979147
prabhash-code <prabhash.code@gmail.com>
2023-12-12 21:31:58 +0100

    Add update scores feature

--- src/__tests__/Scoreboard.test.js
+++ src/__tests__/Scoreboard.test.js
@@ -27,3 +27,39 @@ test('starts a new game', () => {
     const matchElement = screen.getByText(/Team A 0 - 0 Team B/i);
     expect(matchElement).toBeInTheDocument();
 });
+
+test('updates home score', () => {
+  render(<Scoreboard />);
+  const homeTeamInput = screen.getByLabelText(/Home Team:/i);
+  const awayTeamInput = screen.getByLabelText(/Away Team:/i);
+  const startButton = screen.getByText(/Start New Game/i);
+
+  fireEvent.change(homeTeamInput, { target: { value: 'Team A' } });
+  fireEvent.change(awayTeamInput, { target: { value: 'Team B' } });
+  fireEvent.click(startButton);
+
+  const homeScoreButton = screen.getByText('+1 Home');
+
+  fireEvent.click(homeScoreButton);
+
+  const updatedMatchElement = screen.getByText(/Team A 1 - 0 Team B/i);
+  expect(updatedMatchElement).toBeInTheDocument();
+});
+
+test('updates away score', () => {
+  render(<Scoreboard />);
+  const homeTeamInput = screen.getByLabelText(/Home Team:/i);
+  const awayTeamInput = screen.getByLabelText(/Away Team:/i);
+  const startButton = screen.getByText(/Start New Game/i);
+
+  fireEvent.change(homeTeamInput, { target: { value: 'Team A' } });
+  fireEvent.change(awayTeamInput, { target: { value: 'Team B' } });
+  fireEvent.click(startButton);
+  
+  const awayScoreButton = screen.getByText('+1 Away');
+
+  fireEvent.click(awayScoreButton);
+
+  const updatedMatchElement = screen.getByText(/Team A 0 - 1 Team B/i);
+  expect(updatedMatchElement).toBeInTheDocument();
+});
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -16,13 +16,23 @@ const Scoreboard = () => {
         setMatches([...matches, newMatch]);
     };
 
+    const updateScore = (matchId, homeScore, awayScore) => {
+        setMatches(matches.map((match) =>
+            match.id === matchId
+                ? { ...match, homeScore, awayScore }
+                : match
+        ));
+    };
+
     return (
         <div>
             <h2>Ongoing Matches</h2>
             <div>
                 {matches.map((match) => (
                     <div key={match.id}>
+                        <button onClick={() => updateScore(match.id, match.homeScore + 1, match.awayScore)}>+1 Home</button>
                         <span>{match.homeTeam} {match.homeScore} - {match.awayScore} {match.awayTeam}</span>
+                        <button onClick={() => updateScore(match.id, match.homeScore, match.awayScore + 1)}>+1 Away</button>
                     </div>
                 ))}
             </div>
