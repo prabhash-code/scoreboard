7bb64f9c53f6931ac739e672c39652e3fa82329a
prabhash-code <prabhash.code@gmail.com>
2023-12-27 19:48:11 +0100

    support cards information

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -6,6 +6,7 @@ const Scoreboard = () => {
     const [matches, setMatches] = useState([]);
     const [showSummary, setShowSummary] = useState(false);
     const [currentPlayer, setCurrentPlayer] = useState('');
+    const [currentCard, setCurrentCard] = useState('red');
 
     const startNewGame = (homeTeam, awayTeam) => {
         const newMatch = {
@@ -14,6 +15,7 @@ const Scoreboard = () => {
             homeScore: 0,
             awayScore: 0,
             goalDetails: [],
+            cardDetails: [],
             id: Date.now(),
         };
 
@@ -37,26 +39,20 @@ const Scoreboard = () => {
                         awayScore,
                         goalDetails: [
                             ...match.goalDetails,
-                            { time: getGoalTime(timeOfGoal, match.id), player: currentPlayer },
+                            { time: getEventTime(timeOfGoal, match.id), player: currentPlayer },
                         ],
                     }
                     : match
             )
         );
-        
+
         setCurrentPlayer('')
     };
 
-    const getGoalTime = (goalTime, matchStartTime) => {
-        const timeDifference = Math.floor((goalTime - matchStartTime) / (60000));
+    const getEventTime = (eventTime, matchStartTime) => {
+        const timeDifference = Math.floor((eventTime - matchStartTime) / (60000));
         return `${timeDifference}"`;
     };
-
-    const finishGame = (matchId) => {
-        setMatches(matches.filter((match) => match.id !== matchId));
-    };
-
-    const calculateTotalScore = (match) => match.homeScore + match.awayScore;
 
     const getSummary = () => {
         const sortedMatches = [...matches].sort((a, b) => {
@@ -77,9 +73,21 @@ const Scoreboard = () => {
                         ))}
                     </span>
                 )}
+                {match.cardDetails.length > 0 && (
+                    <span>
+                        {match.cardDetails.map((card, idx) => (
+                            <span key={`${card.time}-${card.player}`}>
+                                {idx > 0 || match.goalDetails.length > 0 ? ' ' : ''}
+                                {card.time} ({card.card} - {getInitials(card.player)})
+                            </span>
+                        ))}
+                    </span>
+                )}
             </div>
         ));
     };
+
+    const calculateTotalScore = (match) => match.homeScore + match.awayScore;
 
     const getInitials = (playerName) => {
         const initials = playerName
@@ -89,23 +97,66 @@ const Scoreboard = () => {
         return `(${initials})`;
     };
 
+    const handleIssueCard = (matchId) => {
+        if (!currentPlayer.trim()) {
+            alert('Please enter the player name that scores the goal.');
+            return;
+        }
+        const timeOfCard = Date.now();
+
+        setMatches((prevMatches) =>
+            prevMatches.map((match) =>
+                match.id === matchId
+                    ? {
+                        ...match,
+                        cardDetails: [
+                            ...match.cardDetails,
+                            { time: getEventTime(timeOfCard, match.id), card: currentCard, player: currentPlayer },
+                        ],
+                    }
+                    : match
+            )
+        );
+
+        setCurrentPlayer('')
+    };
+
+    const finishGame = (matchId) => {
+        setMatches(matches.filter((match) => match.id !== matchId));
+    };
+
     return (
         <div className="scoreboard">
             <h2>Ongoing Matches</h2>
             <div>
                 {matches.map((match) => (
                     <div key={match.id} className="match">
-                        <label htmlFor="playerName">Player Name:</label>
-                        <input
-                            type="text"
-                            id="playerName"
-                            value={currentPlayer}
-                            onChange={(e) => setCurrentPlayer(e.target.value)}
-                        />
-                        <button onClick={() => updateScore(match.id, match.homeScore + 1, match.awayScore)}>+1 Home</button>
-                        <span>{match.homeTeam} {match.homeScore} - {match.awayScore} {match.awayTeam}</span>
-                        <button onClick={() => updateScore(match.id, match.homeScore, match.awayScore + 1)}>+1 Away</button>
-                        <button className="finish" onClick={() => finishGame(match.id)}>Finish Game</button>
+                        <div className="score-section">
+                            <label htmlFor="playerName">Player Name:</label>
+                            <input
+                                type="text"
+                                id="playerName"
+                                value={currentPlayer}
+                                onChange={(e) => setCurrentPlayer(e.target.value)}
+                            />
+                            <button onClick={() => updateScore(match.id, match.homeScore + 1, match.awayScore)}>+1 Home</button>
+                            <span>{match.homeTeam} {match.homeScore} - {match.awayScore} {match.awayTeam}</span>
+                            <button onClick={() => updateScore(match.id, match.homeScore, match.awayScore + 1)}>+1 Away</button>
+                            <button className="finish" onClick={() => finishGame(match.id)}>Finish Game</button>
+                        </div>
+                        <div className="card-section">
+                            <label htmlFor="card">Card Type:</label>
+                            <select
+                                id="card"
+                                value={currentCard}
+                                onChange={(e) => setCurrentCard(e.target.value)}
+                            >
+                                <option value={"green"}>Green</option>
+                                <option value="yellow">Yellow</option>
+                                <option value="red">Red</option>
+                            </select>
+                            <button onClick={() => handleIssueCard(match.id)}>Issue Card</button>
+                        </div>
                     </div>
                 ))}
             </div>
